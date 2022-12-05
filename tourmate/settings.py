96be80be0a58301a2b698f91921f881e60703295
richo <richowijayaputra3673@gmail.com>
2022-12-05 15:03:04 +0700

    config

--- tourmate/settings.py
+++ tourmate/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5pec(yljj9gdmgk#u0w4jic27-=+z$hi7v19d1#op9i#lg6b4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'testserver', '10.200.19.217']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'testserver', '10.200.19.217','http://django-env.eba-giv7pm33.us-east-1.elasticbeanstalk.com/']
 
 # Application definition
 
